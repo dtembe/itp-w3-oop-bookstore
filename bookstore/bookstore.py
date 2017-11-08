94d624ca4506709092dbced3527624150357a0a7
Dan Tembe <31318833+dtembe@users.noreply.github.com>
2017-11-08 06:23:33 -0600

    Commit 1 to test initial objects.

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,23 @@
 class Bookstore(object):
-    pass
+
+    def __init__(self, store_name):
+        self.name = store_name
+        self.book_list = []
+
+        self.response = []
 
 
 class Author(object):
-    pass
+
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.book_list = []
 
 
 class Book(object):
-    pass
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.book_list.append(self)
